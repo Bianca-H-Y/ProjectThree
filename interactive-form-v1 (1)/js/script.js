1c0eebd6fbb834376b23e24fb9883b5b1ce7c12f
Bianca-why <56407953+Bianca-why@users.noreply.github.com>
2019-12-08 21:23:21 -0700

    Finsihed the name section

--- interactive-form-v1 (1)/js/script.js
+++ interactive-form-v1 (1)/js/script.js
@@ -2,4 +2,22 @@
 //used CDN link for minified version of jQuery.
 //add style sheet successful:https://teamtreehouse.com/library/css-reset-with-normalize//
 
-console.log('Hey');
+
+/*When the page loads the cursor is in the 'name' field: 
+https://teamtreehouse.com/library/using-on-for-event-handling 
+target the id 'namel' under input with jQuery .focus() */
+$('name').focus();
+
+//use .hide() to hide the HTML add on line 38 of my HTML
+const $otherTitle = $('#other-title'); 
+$('#other-title').hide();
+
+/**Create else if statement that will show the text field if other is selected. 
+ * Use .val for input and bind with .change **/
+$('#title').change(function(){
+    if ($(this).val() === 'other') {
+        $('#other-title').show();
+    } else {
+        $('#other-title').hide();
+    }
+});
