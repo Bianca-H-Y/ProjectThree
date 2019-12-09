00501bde516f74b8263db29de507bf411aea804b
Bianca-why <56407953+Bianca-why@users.noreply.github.com>
2019-12-08 17:27:10 -0700

    Added CSS reset and CDN link

---
+++ interactive-form-v1 (1)/js/script.js
@@ -0,0 +1,5 @@
+//test passed that JS is attached to HTML file: console.log('Hey');//
+//used CDN link for minified version of jQuery.
+//add style sheet successful:https://teamtreehouse.com/library/css-reset-with-normalize//
+
+console.log('Hey');
